c2aa9d34e3e63c0d76b7f35abef1857ef5b37ce0
Mitsuo Heijo <mitsuo.heijo@gmail.com>
2020-08-17 17:41:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# semerrgroup
+
+[![ci](https://github.com/johejo/semerrgroup/workflows/ci/badge.svg?branch=main)](https://github.com/johejo/semerrgroup/actions?query=workflow%3Aci)
+[![PkgGoDev](https://pkg.go.dev/badge/github.com/johejo/semerrgroup)](https://pkg.go.dev/github.com/johejo/semerrgroup)
+[![codecov](https://codecov.io/gh/johejo/semerrgroup/branch/main/graph/badge.svg)](https://codecov.io/gh/johejo/semerrgroup)
+[![Go Report Card](https://goreportcard.com/badge/github.com/johejo/semerrgroup)](https://goreportcard.com/report/github.com/johejo/semerrgroup)
+
+semerrgroup is errgroup wrapper with the limitation of the number of concurrent executions by the semaphore.
+
+Most was stolen from errgroup.
+
+WithContext and Group are exactly the same as those of errgroup.
+
+## Example
+
+```go
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	errgroup "github.com/johejo/semerrgroup"
+)
+
+func ExampleLimitedGroup() {
+	g, ctx := errgroup.WithSemaphore(context.Background(), 2) // only two task run in parallel.
+
+	begin := time.Now()
+	// run 3 tasks
+	for i := 0; i < 3; i++ {
+		g.GoWithAcquire(ctx, func() error {
+			time.Sleep(1 * time.Second)
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	since := time.Since(begin).Round(time.Second)
+	if since != 2*time.Second {
+		log.Fatalf("should pass abount 2 seconds, but passed %v", since)
+	}
+	fmt.Println(since)
+
+	// Output:
+	// 2s
+}
+```
+
+## License
+
+BSD 3-Clause
+
+## Author
+
+Mitsuo Heijo (@johejo)
---
+++ semerrgroup.go
@@ -0,0 +1,114 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// semerrgroup is errgroup with the limitation of the number of concurrent executions by the semaphore.
+//
+// Most was stolen from errgroup.
+//
+// WithContext and Group are exactly the same as those of errgroup.
+package semerrgroup
+
+import (
+	"context"
+	"sync"
+
+	"golang.org/x/sync/semaphore"
+)
+
+// A Group is a collection of goroutines working on subtasks that are part of
+// the same overall task.
+//
+// A zero Group is valid and does not cancel on error.
+type Group struct {
+	cancel func()
+
+	wg sync.WaitGroup
+
+	errOnce sync.Once
+	err     error
+}
+
+// WithContext returns a new Group and an associated Context derived from ctx.
+//
+// The derived Context is canceled the first time a function passed to Go
+// returns a non-nil error or the first time Wait returns, whichever occurs
+// first.
+func WithContext(ctx context.Context) (*Group, context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	return &Group{cancel: cancel}, ctx
+}
+
+// Wait blocks until all function calls from the Go method have returned, then
+// returns the first non-nil error (if any) from them.
+func (g *Group) Wait() error {
+	g.wg.Wait()
+	if g.cancel != nil {
+		g.cancel()
+	}
+	return g.err
+}
+
+// Go calls the given function in a new goroutine.
+//
+// The first call to return a non-nil error cancels the group; its error will be
+// returned by Wait.
+func (g *Group) Go(f func() error) {
+	g.wg.Add(1)
+
+	go func() {
+		defer g.wg.Done()
+
+		if err := f(); err != nil {
+			g.errOnce.Do(func() {
+				g.err = err
+				if g.cancel != nil {
+					g.cancel()
+				}
+			})
+		}
+	}()
+}
+
+// LimitedGroup is a wrapper for Group with semaphore.
+type LimitedGroup struct {
+	*Group
+
+	sem *semaphore.Weighted
+}
+
+// WithSemaphore returns a new LimitedGroup with the given weight.
+func WithSemaphore(ctx context.Context, n int64) (*LimitedGroup, context.Context) {
+	g, ctx := WithContext(ctx)
+	return &LimitedGroup{Group: g, sem: semaphore.NewWeighted(n)}, ctx
+}
+
+// GoWithAcquire calls the given function in a new goroutine.
+//
+// More goroutines than given in WithSemaphore will not start.
+func (g *LimitedGroup) GoWithAcquire(ctx context.Context, f func() error) {
+	if err := g.sem.Acquire(ctx, 1); err != nil {
+		g.errOnce.Do(func() {
+			g.err = err
+			if g.cancel != nil {
+				g.cancel()
+			}
+		})
+		return
+	}
+	g.wg.Add(1)
+
+	go func() {
+		defer g.wg.Done()
+		defer g.sem.Release(1)
+
+		if err := f(); err != nil {
+			g.errOnce.Do(func() {
+				g.err = err
+				if g.cancel != nil {
+					g.cancel()
+				}
+			})
+		}
+	}()
+}
